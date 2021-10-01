f44c8bc227ad3f968d87ffe0a66b6002ba7dd11e
tracer-nju <tracer@nju.edu.cn>
2021-10-02 00:09:37 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: a30539c53af44fe0b48507854d086c4f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:09:37 up  2:02,  1 user,  load average: 0.35, 0.55, 0.37

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,28 @@
 #include <stdint.h>
 
+uint64_t bintran(uint32_t a){
+  uint64_t k=0;                     //记录2的mici
+  while(a>=(1<<k)){
+    k++;
+  }
+  return k-1; 
+}
+uint64_t mod(uint64_t x,uint64_t m){
+  while(x>=m){
+    x=x-m;
+  }
+  return x;
+}
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+  uint64_t x=a<b?a:b,y=a>b?a:b;
+  
+  uint64_t result=0;
+  uint64_t k;
+  while(y>0){
+    k=bintran(y);
+    y=y-(1<<k);
+    result+=mod(x<<k,m);
+  }
+  
+  return result;
 }
