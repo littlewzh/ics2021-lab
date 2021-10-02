9d5ad2f98e1f4caa9280d7902764229d2bde6c70
tracer-nju <tracer@nju.edu.cn>
2021-10-02 10:32:51 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 0fed3162b667420fae7d33f6ab5b68a6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  10:32:51 up 6 min,  1 user,  load average: 0.07, 0.21, 0.12

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -20,10 +20,10 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t k;
   while(y>0){
     k=bintran(y);
-    printf("%ld",k);
+    printf("%lld",k);
     y=y-(1<<k);
     result+=mod(x<<k,m);
-    printf("%ld",result);
+    printf("%lld",result);
   }
   
   return result;
