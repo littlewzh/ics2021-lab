763576eb1891f91146d1c843496acdff0233241e
tracer-nju <tracer@nju.edu.cn>
2021-10-04 20:04:21 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 02d0f4cfec5b406d835aa61a8d861392   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:04:21 up 10:21,  1 user,  load average: 0.00, 0.02, 0.00

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -7,9 +7,16 @@ uint64_t bintran(uint32_t a){
   }
   return k-1; 
 }
-uint64_t mod(uint64_t x,uint64_t m){
+uint64_t mod(uint64_t x,uint64_t y,uint64_t m){
   while(x>=m){
     x=x-m;
+  }
+  while(y>=m){
+     y=y-m;
+   }
+  uint64_t s=x+y;
+  if(s<x){                       //此时发生了溢出
+    mod(s+1,(1<<64)-1,m);
   }
   return x;
 }
@@ -17,11 +24,26 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t x=a<b?a:b,y=a>b?a:b;
   
   uint64_t result=0;
-  uint64_t k;
+  int k,l;
   while(y>0){
     k=bintran(y);
     y=y-(1<<k);
-    result=mod(result+mod(x<<k,m),m);
+    l=bintran(x);
+    if((l+k)<64){
+      result=mod(result,x<<k,m);
+    }
+    else {
+      x=x<<(63-l);
+      int s=(k+l)-64;
+      uint64_t presult=mod(x,0,m);
+      //result=mod(result,x,m);
+      for(int i=0;i<=s;i++){
+        if((presult>>(63-i)&)1) {presult=mod((presult<<1)+1,(1<<64)-1,m);}
+        else {continue;}
+      }
+      result=mod(result,presult,m);
+    }
+
   }
   
   return result;
