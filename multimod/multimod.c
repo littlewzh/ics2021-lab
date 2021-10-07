08b175eae1eef5172d64141c266aa036f824bb18
tracer-nju <tracer@nju.edu.cn>
2021-10-07 21:25:09 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 77d53806e8b54636b7f3852d7a58049e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:25:09 up 45 min,  1 user,  load average: 0.25, 0.14, 0.10

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,15 +1,10 @@
 #include <stdint.h>
 #include<stdio.h>
-uint64_t bintran(uint64_t z){
-
-  uint64_t k=0;
-  uint64_t n=1;                     //记录2的mici
-  while((z>=(n<<k))&&k<64){
-    k++;
-  }
-  return k-1; 
+int ain[128],bin[128],min[128];
+uint64_t bintran(uint64_t z,uint64_t i){
+  return (z>>i)&1;
 }
-uint64_t mod(uint64_t x,uint64_t y,uint64_t m){
+uint64_t addmod(uint64_t x,uint64_t y,uint64_t m){
   while(x>=m){
     x=x-m;
   }
@@ -30,24 +25,47 @@ else {
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t result=0;
   uint64_t k=0;
+  for(uint64_t i=0;i<64;i++){
+    if(bintran(b,i)) {bin[i]=1;k=i;}
+    else {bin[i]=0;}
+  }
   uint64_t l=0;
-  l=bintran(a); 
-  while(b>0){
+  for(uint64_t j=0;j<64;j++){
+    if(bintran(a,j)) {ain[j]=1;l=j;}
+    else {ain[j]=0;}
+  } 
+  /*while(b>0){
     k=bintran(b);
     uint64_t sub=1;
     b=b-(sub<<k);
     if((l+k)<64){
-      result=mod(result,a<<k,m);
+      result=addmod(result,a<<k,m);
     }
     else {
       uint64_t presult=a;
       for(uint64_t i=0;i<k;i++){
-          presult=mod(presult,presult,m);
+          presult=addmod(presult,presult,m);
       }
-      result=mod(presult,result,m);
+      result=addmod(presult,result,m);
     }
 
+  }*/
+  if((l+k)<64){
+    for(uint64_t i=0;i<=k;i++){
+      if(bin[i]){
+        result=addmod(result,a<<k,m);
+      }
+    }
   }
-  
+  else{
+    uint64_t presult=a;
+    for(uint64_t j=0;j<=k;j++){
+      if(bin[j]){
+        for(uint64_t i=0;i<=j;i++){
+           presult=addmod(presult,presult,m);
+         }  
+      }
+    }
+  }
   return result;
 }
