a44f88eac130e0651e20bd907fc9c164a522f18a
tracer-nju <tracer@nju.edu.cn>
2021-11-11 00:06:44 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 89bb7d11688648bd95412443641e2e5d   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:06:45 up  1:55,  1 user,  load average: 0.09, 0.05, 0.01

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -10,31 +10,28 @@ int64_t asm_add(int64_t a, int64_t b) {
 }
 
 int asm_popcnt(uint64_t x) {
-  int s = 0;
+  /*int s = 0;
   for (int i = 0; i < 64; i++) {
     if ((x >> i) & 1) s++;
-  }
-  /*int s=0;
+  }*/
+  int s=0;
   //int i=0;
   asm(
-     //"mov $0x0,%[xs];"
-      "mov %%edx %[as]"
-      "mov $0x0 %[ai]"
-      "mov %%edi, %[ax];"
-     ".loop:"
-      "cmp $0x40,%[ai];"
-      "jge  .ret;"
-      "incl %[ai];"
-      "shr $0x1,%[ax];"
-      "mov %[ax],%%eax;"
+      "mov $0x0,%%ecx;"           //int i=0
+      "mov $0x0,%%edx;"          // int s=0
+      ".loop:"
+      "mov %%rdi,%%rax;"         // x->rdi
+      "shr %%cl,%%rax;"
       "and $0x1,%%eax;"
-      "cmp $0x1,%%eax;"
-      "jz .loop;"
-      "incl %[as];"
-      "jmp .loop;"
-      :[as]"=r"(s)
-      :[as]"r"(s),[ai]"r"(i),[ax]"r"(x)
-  );*/
+      "incl %%ecx;"
+      "add %%eax,%%edx;"
+      "cmp $0x40,%%ecx;"
+      "jl .loop;"
+
+   
+      :"=r"(s)
+      :"r"(s),"r"(x)
+  );
   return s;
 }
 
