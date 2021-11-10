7e1607d82fa40177cee42f61d67c29c0577669d2
tracer-nju <tracer@nju.edu.cn>
2021-11-10 21:48:53 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: d12ebe40e46b4510a3efb7a5e15d0369   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:48:53 up  4:00,  1 user,  load average: 0.36, 0.51, 0.61

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -10,10 +10,29 @@ int64_t asm_add(int64_t a, int64_t b) {
 }
 
 int asm_popcnt(uint64_t x) {
-  int s = 0;
+ /* int s = 0;
   for (int i = 0; i < 64; i++) {
     if ((x >> i) & 1) s++;
-  }
+  }*/
+  int s=0;
+  int i=0;
+  asm(
+     "mov $0x0,%[xs];
+      mov %edi, %[ax];
+     .loop:
+      cmp $0x40,%[ai];
+      jge  .ret;
+      incl %[ai];
+      shr $0x1,%[ax];
+      mov %[ax],%[or];
+      and $0x1,%[or];
+      cmp $0x1,%[or];
+      jz .loop;
+      incl %[xs];
+      jmp .loop;"
+      :[xs]"=r"(s)
+      :[xs]"r"(s),[ai]"r"(i),[ax]"r"(x)
+  );
   return s;
 }
 
