9043e1447c014a4ca471ac40c76e84ba69cb79dd
tracer-nju <tracer@nju.edu.cn>
2021-11-15 19:44:28 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 67109cb8e7f94c54b03a29230590e391   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  19:44:28 up  1:20,  1 user,  load average: 0.72, 0.66, 0.65

--- asm/asm.h
+++ asm/asm.h
@@ -5,7 +5,7 @@
 //#include <setjmp.h> // remove this
 //#define asm_jmp_buf jmp_buf
 typedef unsigned long asm_jmp_buf[6];
-#define asm_jmp_buf* asm_jmp_buf;
+#define  asm_jmp_buf asm_jmp_buf *;
 int64_t asm_add(int64_t a, int64_t b);
 int     asm_popcnt(uint64_t x);
 void   *asm_memcpy(void *dest, const void *src, size_t n);
