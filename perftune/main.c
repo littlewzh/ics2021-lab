22ea56950686a9c20d9b22114f4dc15143ce9056
tracer-nju <tracer@nju.edu.cn>
2021-11-26 14:54:46 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: d573a1d8a90c4f8cb040be61d712a06d   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  14:54:46 up  4:47,  1 user,  load average: 0.14, 0.15, 0.16

--- perftune/main.c
+++ perftune/main.c
@@ -5,7 +5,7 @@ int *sieve(int n);
 
 int main() {
   int *primes = sieve(400);
-  for (int *cur = primes+1, i = 0; *cur; cur++, i++) {
+  for (int *cur = primes, i = 0; *cur; cur++, i++) {
     printf("%8d", *cur);
     if (i % 8 == 7 || !*(cur + 1)) printf("\n");
   }
