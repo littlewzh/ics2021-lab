3e390032165eb4d85c4e684f3d2975d988d7d99e
tracer-nju <tracer@nju.edu.cn>
2021-11-26 14:42:21 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: d573a1d8a90c4f8cb040be61d712a06d   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  14:42:21 up  4:34,  1 user,  load average: 0.26, 0.25, 0.19

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -10,12 +10,12 @@ static int  primes[N];
 
 int *sieve(int n) {
   assert(n + 1 < N);
-  for (int i = 2; i <= n; i++){
+  /*for (int i = 2; i <= n; i++){
     is_prime[i] = true;
     //is_prime[i+1]=false;
   }
   //is_prime[2]=true;
-  /*for (int i = 3; i<n; i++) {
+  for (int i = 3; i<n; i++) {
     if(is_prime[i]){
        for (int j = i*i; j < n; j += i) {
         is_prime[j] = false;
@@ -29,16 +29,14 @@ int *sieve(int n) {
       *p++ = i;
     }
   *p = 0;*/
-  int *p=primes;
-  int cnt=0;
-  for(int i=2; i<=n; i++){
-	if(is_prime[i]){
-	  *(p+cnt)=i;cnt++;}
-	for(int j=1; j<=cnt&&i*(*(p+j))<=n; j++){
-	  is_prime[i*(*(p+j))]=false;
-	  if(i%(*(p+j))==0) {break;}
-	} 
-}
- *p=0;
+	for(int i=2; i<=n; i++){
+		if(is_prime[i])
+			primes[++cnt] = i ;
+		for(int j=1; j<=cnt&&i*primes[j]<=n; j++){
+			is_prime[i*primes[j]]=false;
+			if(i%primes[j]==0)
+				break;
+		} 
+	}
   return primes;
 }
