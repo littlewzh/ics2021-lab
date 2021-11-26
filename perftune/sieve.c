0499020f25ab9cf261ecac4bbe08c67c85cb3f9d
tracer-nju <tracer@nju.edu.cn>
2021-11-26 13:35:01 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: d573a1d8a90c4f8cb040be61d712a06d   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  13:35:01 up  3:27,  1 user,  load average: 0.22, 0.18, 0.12

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -13,9 +13,9 @@ int *sieve(int n) {
   for (int i = 0; i <= n; i++)
     is_prime[i] = true;
     
-  for (int i = 2; i <= n; i++) {
+  for (int i = 2; i*i<= n; i++) {
     if(is_prime[i]){
-       for (int j = i + i; j <= n; j += i) {
+       for (int j = i*i; j <= n; j += i) {
         is_prime[j] = false;
        }
     }
