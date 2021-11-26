a668b5ddce705f78798a81269a824b7cc96e3b54
tracer-nju <tracer@nju.edu.cn>
2021-11-26 12:25:31 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: d573a1d8a90c4f8cb040be61d712a06d   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  12:25:31 up  2:17,  1 user,  load average: 0.10, 0.07, 0.08

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -100,7 +100,7 @@ void asm_longjmp(asm_jmp_buf env, int val) {
 //	"mov 32(%%rdi),%%rdi\n\t"
 //	"mov %%rsi,40(%%rdi)\n\t"
 //        "mov 64(%%rdi),%%rbx\n\t"
-	"jmp 0x00000000000011d9\n\t"
+	"call 0x00000000000011d9\n\t"
         "ret\n\t"					//pop eip
 	:
 	:"r"(val)
