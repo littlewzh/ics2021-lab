4faaaad63a5c7a14bfddaf1c5fcb7a85b223fc19
tracer-nju <tracer@nju.edu.cn>
2021-12-14 01:21:44 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 56f2f11cb3434cb8b4be30107d96b669   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  01:21:45 up  1:26,  1 user,  load average: 0.52, 0.29, 0.21

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -20,7 +20,7 @@ void cycle_increase(int n) { cycle_cnt += n; }
 // TODO: implement the following functions
 
 uint32_t cache_read(uintptr_t addr) {
-  uint32_t tag_in=addr>>(BLOCK_WIDTH+set_num);
+  uint32_t tag_in=addr>>12;
   uint32_t index=(addr&0xfff)>>6;//(addr<<(32-BLOCK_WIDTH-set_num))>>(32-set_num);
   printf("0x%08x\n",index);
   uint32_t offset=addr&0x3c;
@@ -44,6 +44,7 @@ uint32_t cache_read(uintptr_t addr) {
     printf("reach here\n");
     mem_read(addr>>BLOCK_WIDTH,(uint8_t *)(cac+(4*index+k)*64));
     valid[index*4+k]=1;
+    tag[index*4+k]=tag_in;
     dirty[index*4+k]=0;
     data_out=((uint32_t)cac[(4*index+k)*64+offset])|((uint32_t)cac[(4*index+k)*64+offset+1]<<8)|((uint32_t)cac[(4*index+k)*64+offset+2]<<16)|((uint32_t)cac[(4*index+k)*64+offset+3]<<24);
   }
@@ -53,7 +54,7 @@ uint32_t cache_read(uintptr_t addr) {
 }
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
-  uint32_t tag_in=addr>>(BLOCK_WIDTH+set_num);
+  uint32_t tag_in=addr>>12;//(BLOCK_WIDTH+set_num);
   uint32_t index=(addr&0xfff)>>6;//(addr<<(32-BLOCK_WIDTH-set_num))>>(32-set_num);
   printf("0x%08x\n",index);
   uint32_t offset=addr&0x3c;
@@ -84,6 +85,7 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
     mem_read(addr>>BLOCK_WIDTH,(uint8_t *)(cac+(4*index+k)*64));
     valid[index*4+k]=1;
     dirty[index*4+k]=1;
+    tag[index*4+k]=tag_in;
     cac[(4*index+k)*64+offset]=data&0xff;
     cac[(4*index+k)*64+offset+1]=data&0xff00>>8;
     cac[(4*index+k)*64+offset+2]=data&0xff0000>>16;
