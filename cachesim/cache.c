e635407b74e0bcc1719b6ac9671b9e995eca976f
tracer-nju <tracer@nju.edu.cn>
2021-12-14 00:02:02 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 56f2f11cb3434cb8b4be30107d96b669   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:02:02 up 7 min,  1 user,  load average: 0.23, 0.33, 0.22

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -5,19 +5,89 @@ void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
 
 static uint64_t cycle_cnt = 0;
-
+static int ass;
+static int total;
+static int set_num;
+static int hang_num;
+static uint8_t cac[exp2(14)];
+static bool valid[exp2(14-BLOCK_WIDTH)];           //每行的有效位
+static uint32_t tag[exp2(14-BLOCK_WIDTH)];           //每行的标志位
+static dirty[exp2(14-BLOCK_WIDTH)];              //每行的dirty位
 void cycle_increase(int n) { cycle_cnt += n; }
 
 // TODO: implement the following functions
 
 uint32_t cache_read(uintptr_t addr) {
-  return 0;
+  int tag_in=addr>>(BLOCK_WIDTH+set_num);
+  int index=(addr<<(32-BLOCK_WIDTH-set_num))>>(32-set_num);
+  int offset=addr&0x3c;
+  int hit=0;
+  uint32_t data_out;
+  int i;
+  for(i=0;i<ass;i++){
+    if((tag[index*4+i]==tag_in)&&valid[index*4+i]) hit=1;break;
+  }
+  if(hit==1){
+    data_out=((uint32_t)cac[(4*index+i)*64+offset])|((uint32_t)cac[(4*index+i)*64+offset+1]<<8)|((uint32_t)cac[(4*index+i)*64+offset+2]<<16)|((uint32_t)cac[(4*index+i)*64+offset+3]<<24);
+  }
+  else{
+    int k=choose(ass);
+    if(dirty[index*4+k]==1){
+      mem_write((tag[index*4+k]<<set_num)|(index*4+k),(uint8_t *)(cac+(4*index+k)*64));
+    }
+    mem_read(addr>>BLOCK_WIDTH,(uint8_t *)(cac+(4*index+k)*64));
+    valid[index*4+k]=1;
+    dirty[index*4+k]=0;
+    data_out=((uint32_t)cac[(4*index+k)*64+offset])|((uint32_t)cac[(4*index+k)*64+offset+1]<<8)|((uint32_t)cac[(4*index+k)*64+offset+2]<<16)|((uint32_t)cac[(4*index+k)*64+offset+3]<<24);
+  }
+  return data_out;
 }
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
+  int tag_in=addr>>(BLOCK_WIDTH+set_num);
+  int index=(addr<<(32-BLOCK_WIDTH-set_num))>>(32-set_num);
+  int offset=addr&0x3c;
+  int hit=0;
+  uint32_t data_in;
+  //uint32_t data_out;
+  int i;
+  for(i=0;i<ass;i++){
+    if((tag[index*4+i]==tag_in)&&valid[index*4+i]) hit=1;break;
+  }
+  if(hit==1){
+    dirty[index*4+i]=1;
+    cac[(4*index+i)*64+offset]=data&0xff;
+    cac[(4*index+i)*64+offset+1]=data&0xff00>>8;
+    cac[(4*index+i)*64+offset+2]=data&0xff0000>>16;
+    cac[(4*index+i)*64+offset+3]=data&0xff000000>>24;
+    //data_out=((uint32_t)cac[(4*index+i)*64+offset])|((uint32_t)cac[(4*index+i)*64+offset+1]<<8)|((uint32_t)cac[(4*index+i)*64+offset+2]<<16)|((uint32_t)cac[(4*index+i)*64+offset+3]<<24);
+  }
+  else{
+    int k=choose(ass);
+    if(dirty[index*4+k]==1){
+      mem_write((tag[index*4+k]<<set_num)|(index*4+k),(uint8_t *)(cac+(4*index+k)*64));
+    }
+    mem_read(addr>>BLOCK_WIDTH,(uint8_t *)(cac+(4*index+k)*64));
+    valid[index*4+k]=1;
+    dirty[index*4+k]=1;
+    cac[(4*index+k)*64+offset]=data&0xff;
+    cac[(4*index+k)*64+offset+1]=data&0xff00>>8;
+    cac[(4*index+k)*64+offset+2]=data&0xff0000>>16;
+    cac[(4*index+k)*64+offset+3]=data&0xff000000>>24;
+    //data_out=((uint32_t)cac[(4*index+k)*64+offset])|((uint32_t)cac[(4*index+k)*64+offset+1]<<8)|((uint32_t)cac[(4*index+k)*64+offset+2]<<16)|((uint32_t)cac[(4*index+k)*64+offset+3]<<24);
+  }
+  //return data_out;
 }
 
 void init_cache(int total_size_width, int associativity_width) {
+  total=exp2(total_size_width);                                  //cache total size;
+  ass=exp2(associativity_width);                                  // the number of line for each set;
+  set_num=total_size_width-BLOCK_WIDTH-associativity_width;  //组数
+  hang_num=total_size_width-BLOCK_WIDTH;                   //行数
+  //uint8_t cac[exp2(total_size_width)];
+  //uint32_t path=exp2(associativity_width);
+  //bool valid[exp2(total_size_width-BLOCK_WIDTH)];           //每行的有效位
+  //uint32_t tag[exp2(total_size_width-BLOCK_WIDTH)];           //每行的标志位
 }
 
 void display_statistic(void) {
