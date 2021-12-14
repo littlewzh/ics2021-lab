aba58cefb63f1c7b06ce743eaab05650a7a489e3
tracer-nju <tracer@nju.edu.cn>
2021-12-14 18:57:42 +0800

    Static hostname: little-bird          Icon name: computer-laptop            Chassis: laptop         Machine ID: cdaf3329ddcf420fb5abab034062b98f            Boot ID: 1ad3006b259743b799ede33d60836e58   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  18:57:43 up 27 min,  1 user,  load average: 0.37, 0.45, 0.51

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -23,6 +23,7 @@ void cycle_increase(int n) { cycle_cnt += n; }
 uint32_t cache_read(uintptr_t addr) {
   uint32_t addrin=addr&~0x3;
   uint32_t tag_in=addr>>(BLOCK_WIDTH+set_num);
+  printf("0x%08x\n",tag_in);
   uint32_t index=((addr<<(32-BLOCK_WIDTH-set_num))>>(32-set_num));
   printf("%d\n",set_num);
   printf("0x%08x\n",index);
